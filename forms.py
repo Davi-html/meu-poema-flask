2db4c03cc9ea4918451dee7a6f60caa68fb96261
Davi-html <davifernandes3674@gmail.com>
2025-01-04 12:52:43 -0300

    finalizando a tela de signup

---
+++ forms.py
@@ -0,0 +1,17 @@
+import flask_wtf
+from wtforms import StringField, SubmitField, PasswordField, EmailField, BooleanField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
+
+class SignupForm(flask_wtf.FlaskForm):
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    name = StringField('Name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Submit')
+
+
+class SigninForm(flask_wtf.FlaskForm):
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
+    RememberPassword = BooleanField('Lembrar dados de acesso')
+    submit = SubmitField('Submit')
--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 from flask import Flask, render_template
 from flask_avatars import Avatars
+from forms import SignupForm, SigninForm
 
 app = Flask(__name__)
 avatars = Avatars(app)
 
+app.config['SECRET_KEY'] = '21737b39a5ceb6794d89e31a745de937'
+
 @app.route("/")
-def hello_world():
+def home():
     return render_template('home.html', avatars=avatars)
+
+
+@app.route("/signup")
+def signup():
+    formsignup = SignupForm()
+    return render_template('signup.html', formsignup=formsignup)
+
+
+@app.route("/signin")
+def signin():
+    formsignin = SigninForm()
+    return render_template('signin.html', formsignin=formsignin)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
