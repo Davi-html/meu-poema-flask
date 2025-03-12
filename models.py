d2636c5bbf00bafa4578188b0fb0d735831da37b
Davi-html <davifernandes3674@gmail.com>
2025-03-11 22:09:47 -0300

    relacionando post e users

--- models.py
+++ models.py
@@ -8,10 +8,12 @@ class User(database.Model):
     email = database.Column(database.String(), unique=True, nullable=False)
     password = database.Column(database.String(),nullable=False)
     foto_perfil = database.Column(database.String(), default='default.jpg')
-
+    posts = database.relationship('Post', backref='author', lazy=True)
+    bio = database.Column(database.String(), default='Sem biografia')
 
 class Post(database.Model):
     id = database.Column(database.Integer, primary_key=True)
     title = database.Column(database.String(), nullable=False)
     content = database.Column(database.Text(), nullable=False)
     date_posted = database.Column(database.DateTime(), nullable=False, default=datetime.utcnow)
+    user_id = database.Column(database.Integer, database.ForeignKey('user.id'), nullable=False)
